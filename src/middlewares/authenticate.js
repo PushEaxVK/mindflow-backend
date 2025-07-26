e76442ff22aa11f85f96b6e1a0f5c1991217f83d
amurovnaa <151229291+amurovnaa@users.noreply.github.com>
2025-07-27 01:10:42 +0400

    fix auth middlware + add docs for auth middleware, errors (#6)

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,41 +1,30 @@
 import createHttpError from 'http-errors';
-import { SessionsCollection } from '../db/models/sessions.js';
+import jwt from 'jsonwebtoken';
 import { UsersCollection } from '../db/models/users.js';
+import { ENV_VARS } from '../constants/envVars.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
-
   if (!authHeader) {
     throw createHttpError(401, 'Please provide Authorization header');
   }
 
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
-
+  const [bearer, token] = authHeader.split(' ');
   if (bearer !== 'Bearer' || !token) {
     throw createHttpError(401, 'Auth header should be of type Bearer');
   }
 
-  const session = await SessionsCollection.findOne({ accessToken: token });
+  const payload = jwt.verify(token, ENV_VARS.JWT_ACCESS_SECRET);
 
-  if (!session) {
-    throw createHttpError(401, 'Session not found');
-  }
-
-  const isAccessTokenExpired =
-    new Date() > new Date(session.accessTokenValidUntil);
-
-  if (isAccessTokenExpired) {
-    throw createHttpError(401, 'Access token expired');
-  }
-
-  const user = await UsersCollection.findById(session.userId);
-
+  const user = await UsersCollection.findById(payload.userId);
   if (!user) {
     throw createHttpError(401, 'User not found');
   }
 
-  req.user = user;
+  if (req.params.userId && req.params.userId !== user._id.toString()) {
+    throw createHttpError(403, 'Forbidden: access denied for this userId');
+  }
 
+  req.user = user;
   next();
 };
