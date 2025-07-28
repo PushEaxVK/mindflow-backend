3c0cace18b87078eaa96829affc6d60cf627964a
PushEax <pusheax.vk@gmail.com>
2025-07-29 02:35:17 +0300

    fix recomend route

--- src/routers/articlesRouter.js
+++ src/routers/articlesRouter.js
@@ -7,65 +7,38 @@ import {
   fetchAllArticles,
   createSingleArticle,
   fetchSavedArticles,
-  updateArticleById,        
+  updateArticleById,
   deleteArticleById,
   saveArticle,
   removeSavedArticle,
-  fetchPopularArticles
+  fetchPopularArticles,
 } from '../controllers/articlesController.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
-
 router.get('/', fetchAllArticles);
-
 
 router.get('/popular', fetchPopularArticles);
 
-
 router.post('/create', authenticate, createSingleArticle);
-
 
 router.patch('/:id', authenticate, updateArticleById);
 
-
 router.delete('/:id', authenticate, deleteArticleById);
-
 
 router.post('/:id/save', authenticate, saveArticle);
 
-
 router.delete('/:id/save', authenticate, removeSavedArticle);
-
 
 router.get('/saved', authenticate, fetchSavedArticles);
 
+router.get('/recommend', fetchRecommendedArticles);
 
 router.get('/:id', fetchArticleById);
 
-
-router.get('/recommend', fetchRecommendedArticles);
-
-
 router.post('/', createManyArticles);
-
 
 router.delete('/all', deleteAllArticles);
 
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
