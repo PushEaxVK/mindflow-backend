8fe8d1c8f0bedf51700274fb17dd34a4dcc5c3db
IhorBut737 <igor.klad@gmail.com>
2025-07-31 22:52:30 +0300

    get-user-api

--- src/services/users.js
+++ src/services/users.js
@@ -9,9 +9,11 @@ export const getUserArticles = async (userId, page = 1, limit = 10) => {
   const skip = (page - 1) * limit;
 
   const articles = await ArticlesCollection.find({ author: userId })
+    .populate('author', 'name')
     .sort({ createdAt: -1 })
     .skip(skip)
-    .limit(limit);
+    .limit(limit)
+    .lean();
 
   const total = await ArticlesCollection.countDocuments({ author: userId });
 
